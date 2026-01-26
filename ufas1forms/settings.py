7be37fd1ead8b87442c53433238c544f748075cd
Djellal Elkacem <djellal.elkacem@gmail.com>
2026-01-26 11:31:33 +0000

    Change default language code from English to Arabic

--- ufas1forms/settings.py
+++ ufas1forms/settings.py
@@ -71,7 +71,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ar'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
